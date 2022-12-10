05c4c9c0b0282a70ccffb22fb94520f55e3a9b35
heph3astus <kiernanolson@gmail.com>
2022-12-10 16:56:49 -0500

    basic API generation

--- pkg/swagger/gen.go
+++ pkg/swagger/gen.go
@@ -2,4 +2,4 @@ package swagger
 
 //go:generate rm -rf server
 //go:generate mkdir -p server
-//go:generate swagger generate server --quiet --target serve
+//go:generate swagger generate server --quiet --target server --name skewax-backend --spec swagger.yml --exclude-main
